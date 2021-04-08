9cba15ba258376e77c57848b40e347f2109c29f6
brunda <36494174+BrundaBR@users.noreply.github.com>
2021-04-08 13:04:00 +0530

    08-04-21 Minimum and maximum in array

--- array/MinandMax.py
+++ array/MinandMax.py
@@ -3,20 +3,36 @@ from os import sys
 sys.stdout = open('output.txt', 'w')
 sys.stdin = open('input.txt', 'r')
 import heapq
+
 '''
 Max and min --O(n)
-add all elements to minheap top element is min
-multiply heap with -1
-and return min again x-1 which is max
-'''
+METHOD 1 (Simple Linear Search) 
+Initialize values of min and max as minimum and maximum of the first two elements respectively. Starting from 3rd, compare each element with max and min, and change max and min accordingly (i.e., if the element is smaller than min then change min, else if the element is greater than max then change max, else ignore the element
 
+'''
 def maxMin(a):
-    heap=[]
-    for i in a:
-        heapq.heappush(heap,i)
-    min_val=heap[0]
-    max_val=heap[-1]
-    return min_val,max_val
     
+    # init min and max
+    if len(a)>2:
+        minimum=a[0]
+        maximum=a[1]
+        for i in range(2,len(a)):
+            if a[i]<minimum:
+                minimum=a[i]
+            elif a[i]>maximum:
+                maximum=a[i]
+        return minimum,maximum
+    elif len(a)==1:
+        return 0
+    elif len(a)<=2:
+        if a[0] < a[1]:
+            minimum=a[0]
+            maximum=a[1]
+            return minimum,maximum
+        else:
+            minimum=a[1]
+            maximum=a[0]
+            return minimum,maximum
+
 input_array=list(map(int,input().split()))
 print(maxMin(input_array))
